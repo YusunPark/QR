22a12624c8fbd2f7ee04cde7c0500edb7c1d03bd
Yusun <solarpy@hanyang.ac.kr>
2022-05-10 11:11:28 +0900

    Change css file to styled components

--- qr_item/src/App.js
+++ qr_item/src/App.js
@@ -1,22 +1,27 @@
 import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 
-import Create from "./routes/Create";
 import { Carousel, Button, Form } from "react-bootstrap";
-
 import { useNavigate } from "react-router-dom";
+
 import styled from "styled-components";
-import "./styles/App.css";
+import Create from "./routes/Create";
 
 function App() {
-  let [name, submitName] = useState('')
-  let [phone, submitPhone] = useState('')
+  let [name, submitName] = useState("");
+  let [phone, submitPhone] = useState("");
 
   let navigate = useNavigate();
 
   let Img = styled.img`
     padding: 10px;
     height: 20rem;
+  `;
+
+  let CenterForm = styled.div`
+    text-align: left;
+    padding-left: 6rem;
+    padding-right: 6rem;
   `;
 
   return (
@@ -68,50 +73,50 @@ function App() {
                   </Carousel.Caption>
                 </Carousel.Item>
               </Carousel>
-
-              <Form className="form-box">
-                <h4>QR 생성을 위해서 정보를 입력해주세요!</h4>
-                <Form.Group className="mb-3" controlId="formName">
-                  <Form.Label column sm="3">
-                    이름
-                  </Form.Label>
-                  <Form.Control
-                    className="input-box"
-                    onChange={(e) => {
-                      submitName(e.target.value);
-                      console.log(e.target.value);
-                      console.log(name);
+              <CenterForm>
+                <Form>
+                  <h4>QR 생성을 위해서 정보를 입력해주세요!</h4>
+                  <Form.Group className="mb-3" controlId="formName">
+                    <Form.Label column sm="3">
+                      이름
+                    </Form.Label>
+                    <Form.Control
+                      className="input-box"
+                      onChange={(e) => {
+                        submitName(e.target.value);
+                        console.log(e.target.value);
+                        console.log(name);
+                      }}
+                      type="text"
+                      id="inputName"
+                      placeholder="Name"
+                    />
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="forNumber">
+                    <Form.Label column sm="3">
+                      전화번호
+                    </Form.Label>
+                    <Form.Control
+                      className="input-box"
+                      onChange={(e) => {
+                        submitPhone(e.target.value);
+                      }}
+                      type="text"
+                      id="inputPhoneNum"
+                      placeholder="Phone Number"
+                    />
+                  </Form.Group>
+                  <Button
+                    variant="primary"
+                    onClick={() => {
+                      navigate("/create");
                     }}
-                    type="text"
-                    id="inputName"
-                    placeholder="Name"
-                  />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="forNumber">
-                  <Form.Label column sm="3">
-                    전화번호
-                  </Form.Label>
-                  <Form.Control
-                    className="input-box"
-                    onChange={(e) => {
-                      submitPhone(e.target.value);
-
-                    }}
-                    type="text"
-                    id="inputPhoneNum"
-                    placeholder="Phone Number"
-                  />
-                </Form.Group>
-                <Button
-                  variant="primary"
-                  onClick={() => {
-                    navigate("/create");
-                  }}
-                  type="submit"
-                >
-                  QR 코드 생성하기
-                </Button>
-              </Form>
+                    type="submit"
+                  >
+                    QR 코드 생성하기
+                  </Button>
+                </Form>
+              </CenterForm>
             </>
           }
         />
